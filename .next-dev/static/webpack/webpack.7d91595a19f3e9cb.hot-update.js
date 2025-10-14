6cf0a7b36e54cc7c83dc49bd9e5bb7355dbbee6f
Zachary <zacharylinz1013@gmail.com>
2025-10-14 16:21:45 -0500

    Add /report page to Next.js pages directory

--- .next-dev/static/webpack/webpack.7d91595a19f3e9cb.hot-update.js
+++ .next-dev/static/webpack/webpack.7d91595a19f3e9cb.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "56dfa908e8917799"; }
+/******/ 	__webpack_require__.h = function() { return "6729ed7634e8083f"; }
 /******/ }();
 /******/ 
 /******/ }
