aa8dfd62e54e19c03f5d801dfe24402d521a5cbd
Zachary <zacharylinz1013@gmail.com>
2025-10-14 12:57:09 -0500

    report

--- pages/rules.js
+++ pages/rules.js
@@ -1,4 +1,5 @@
 import Layout from '../components/Layout';
+import Link from 'next/link';
 
 export default function Rules() {
   return (
@@ -10,6 +11,11 @@ export default function Rules() {
           <li>Mark sensitive images NSFW.</li>
           <li>No illegal content.</li>
         </ul>
+        <div style={{marginTop: 16}}>
+          <Link href="/report" aria-label="Report offensive content" title="Report offensive content">
+            <button type="button">Report</button>
+          </Link>
+        </div>
       </section>
     </Layout>
   );
