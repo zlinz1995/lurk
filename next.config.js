657f638e3b21499f34918240ef1da853ec465e54
Zachary Linz <zacharylinz1013@gmail.com>
2025-11-26 04:24:34 -0600

    Configure static export and backend start script

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: '.next-dev',
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: false,
-  output: 'export',
-  images: { unoptimized: true },
+  // ❗ Do NOT set distDir for static exports unless you REALLY know why.
   // Avoid writing the build dependency trace file that OneDrive can lock
   // See: https://nextjs.org/docs/messages/production-start-no-build-id
   outputFileTracing: false,
